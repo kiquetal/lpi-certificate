96789c14dc4d90fd8b491ab405b4925fcb1532d4
kiquetal <kiquetal@gmail.com>
2017-02-22 09:39:24 -0300

    learning bash script

---
+++ lpi-part1/practical-lpi-book/case.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'Favorite car'
+read CAR
+case $CAR in 
+ volvo | ford ) echo 'Nice I love the:' $CAR
+ ;;
+ porshe | vw ) echo $CAR 'OK'
+ ;;
+ yugo | fiat ) echo 'Sorry'
+ ;;
+ * ) echo 'Not familiar'
+ ;;
+esac
+exit 0
---
+++ lpi-part1/practical-lpi-book/exercises.md
@@ -0,0 +1,6 @@
+#-e will test for the existence	of the new path
+#-d will confirm that target is a directory
+#-r is readable for the current user
+#test $text1 != $text2
+# if [-e "$NEW PATH"]; then
+# fi
